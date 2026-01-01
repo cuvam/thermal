d3949794339657973065495eddaa0b9747a5e5ea
Matt Cuvanov <42915392+cuvam@users.noreply.github.com>
2026-01-01 09:09:13 -0500

    Replace tempwrite program path with direct call
    README specifies it should be placed in $PATH anyway, so making the user replace this with the exact path isn't required. Users can easily revert this change and not put the C programs in the $PATH if it's not desired.

--- gettemp.sh
+++ gettemp.sh
@@ -5,7 +5,7 @@ for i in {1..60}; do
     exec 200>"$LOCKFILE"
     if flock -n 200; then
         TEMP=$(sensors | awk '/Package id 0/{print $4}' | tr -d '+°C' | cut -d'.' -f1)
-        /path/to/tempwrite/program "$TEMP"
+        tempwrite "$TEMP"
     fi
     [ $i -lt 60 ] && sleep 1
 done
